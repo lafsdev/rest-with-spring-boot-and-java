bc2616c3b0250937a4584910444c56e173348ab9
Leandro <lafs.dev@gmail.com>
2023-01-04 23:20:21 -0300

    Testando serialização do JSON

--- api-gateway/src/main/java/io/github/com/lafsdev/apigateway/data/vo/v1/PersonVO.java
+++ api-gateway/src/main/java/io/github/com/lafsdev/apigateway/data/vo/v1/PersonVO.java
@@ -1,15 +1,23 @@
 package io.github.com.lafsdev.apigateway.data.vo.v1;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.io.Serializable;
 
+@JsonPropertyOrder({"id", "address", "first_name", "last_name", "gender"})
 public class PersonVO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private Long id;
+    @JsonProperty("first_name")
     private String firstName;
+    @JsonProperty("last_name")
     private String lastName;
     private String address;
+    @JsonIgnore
     private String gender;
 
     public PersonVO() {}
