71c80d59c25685065d66c22659bb8375202e298c
Leandro Alves de Fontes Silva <lafs.dev@gmail.com>
2023-01-09 16:36:37 -0300

    Ajustes finais no WebMvcConfigurer

--- api-gateway/src/main/java/io/github/com/lafsdev/apigateway/config/WebConfig.java
+++ api-gateway/src/main/java/io/github/com/lafsdev/apigateway/config/WebConfig.java
@@ -21,11 +21,29 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.favorParameter(true)
-                .parameterName("mediaType").ignoreAcceptHeader(true)
+        // https://www.baeldung.com/spring-mvc-content-negotiation-json-xml
+        // Via EXTENSION. http://localhost:8080/api/person/v1.xml DEPRECATED on SpringBoot 2.6
+
+        // Via QUERY PARAM. http://localhost:8080/api/person/v1?mediaType=xml
+		/*
+		configurer.favorParameter(true)
+			.parameterName("mediaType").ignoreAcceptHeader(true)
+			.useRegisteredExtensionsOnly(false)
+			.defaultContentType(MediaType.APPLICATION_JSON)
+				.mediaType("json", MediaType.APPLICATION_JSON)
+				.mediaType("xml", MediaType.APPLICATION_XML);
+		*/
+
+        // Via HEADER PARAM. http://localhost:8080/api/person/v1
+
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
-                .mediaType("xml", MediaType.APPLICATION_XML);
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML)
+        ;
     }
+
 }
